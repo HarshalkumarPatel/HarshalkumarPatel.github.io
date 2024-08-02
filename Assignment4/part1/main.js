98eed58eb7589853754a263dfc5983ab568dcd8d
HarshalkumarPatel <harshalkumar.patel@dcmail.ca>
2024-08-02 17:38:22 -0400

    randomValueFromArray Function Created
    - randomvaluefromarray function created

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -2,3 +2,21 @@
 // File: main.js
 // Date: 25-July-2024
 // Descripation: Silly Story Generator
+
+// 
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+// Function to return random value from array.
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// Create const for story text and placeholder array
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
